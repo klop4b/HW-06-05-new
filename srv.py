52da2db3f8748912b99ba8ff3574ab89ea2db3ae
klop4b <vano.yatsyn@gmail.com>
2020-06-22 13:18:19 +0300

    big remastering 22-06 exceptions read from file path to file

--- srv.py
+++ srv.py
@@ -1,31 +1,80 @@
 import os
 import socketserver
+from datetime import datetime
 from http.server import SimpleHTTPRequestHandler
+from pathlib import Path
 from typing import Dict
 from urllib.parse import parse_qs
-from datetime import datetime
-
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT = {PORT}")
+
+PROJECT_DIR = Path(__file__).parent.resolve()
+print(f"PROJECT_DIR = {PROJECT_DIR}")
+
+TEMPLATES_DIR = PROJECT_DIR / "templates"
+print(f"TEMPLATES_DIR = {TEMPLATES_DIR}")
+
+class NotFound(Exception):
+    pass
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         path, *_qs = self.path.split("?") if '?' in self.path else [self.path, ""]
         print(path)
 
-
         handlers = {
             "/goodbye": self.say_goodbye,
             "/hello": self.say_hello,
-            }
+            "/projects": self.show_projects,
+            "/resume": self.show_resume,
+        }
 
         default_handler = super().do_GET
 
         handler = handlers.get(path, default_handler)
-        handler()
 
-    def say_hello(self):
+        try:
+            handler()
+        except NotFound:
+            print("NotFound")
+            self.respond_404()
+
+    def show_projects(self) -> None:
+        html = TEMPLATES_DIR / "projects" / "index.html"
+        contents = self.get_file_contents(html)
+
+        print("method name: show_projects")
+        print(f"value html: {html}")
+
+        self.respond(contents, "text/html")
+
+    def show_resume(self) -> None:
+        html = TEMPLATES_DIR / "resume" / "index.html"
+        contents = self.get_file_contents(html)
+
+        print("method name: show_resume")
+        print(f"value html: {html}")
+
+        self.respond(contents, "text/html")
+
+    def get_file_contents(self, fp: Path) -> str:
+        print("method name: get_file_contents")
+        print(f"value fp: {fp}")
+
+
+        if not fp.is_file():
+            raise NotFound
+
+        with fp.open("r") as src:
+            ct = src.read()
+
+        print(f"value contents: {ct}")
+
+
+        return ct
+
+    def say_hello(self) -> None:
         args = self.build_query_args()
         name = self.get_name(args)
         year = self.get_year(args)
@@ -40,7 +89,7 @@ class MyHandler(SimpleHTTPRequestHandler):
         print(msg)
         self.respond(msg)
 
-    def say_goodbye(self):
+    def say_goodbye(self) -> None:
         print("say_goodbye")
         if datetime.now().hour in range(13):
             msg = "Goodbye!"
@@ -80,7 +129,6 @@ class MyHandler(SimpleHTTPRequestHandler):
         else:
             return 'the best'
 
-
     def extract_path(self) -> str:
         if self.path.find("hello"):
             return "/hello"
@@ -89,13 +137,23 @@ class MyHandler(SimpleHTTPRequestHandler):
         else:
             return None
 
-    def respond(self, msg: str) -> None:
+    def respond(self, msg: str, сontent_type="text/plain") -> None:
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", сontent_type)
         self.send_header("Content-length", len(msg))
         self.end_headers()
 
         self.wfile.write(msg.encode())
+
+    def respond_404(self) -> None:
+        msg = "Ooops! Nothing found!"
+        self.send_response(404)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+
+        self.wfile.write(msg.encode())
+
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("it works")
